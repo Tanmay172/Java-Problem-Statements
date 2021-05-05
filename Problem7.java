b2c9bfaa0d873eeaae308520a628565f5990bf61
tanmay <tanmaykanwar91@gmail.com>
2021-05-05 19:38:49 +0530

    A Generics question from Hackerrank to display any type of array from a single method

---
+++ Problem7.java
@@ -0,0 +1,38 @@
+
+import java.io.IOException;
+import java.lang.reflect.Method;
+
+class Printer
+{
+   //Write your code here
+	static <T> void printArray(T[] y)
+	{
+		for(int i=0;i<y.length;i++)
+		{
+			System.out.println(y[i]);
+		}
+	} 
+}
+
+class Solution {
+
+
+    public static void main( String args[] ) {
+        Printer myPrinter = new Printer();
+        Integer[] intArray = { 1, 2, 3 };
+        String[] stringArray = {"Hello", "World"};
+        myPrinter.printArray(intArray);
+        myPrinter.printArray(stringArray);
+        int count = 0;
+
+        for (Method method : Printer.class.getDeclaredMethods()) {
+            String name = method.getName();
+
+            if(name.equals("printArray"))
+                count++;
+        }
+
+        if(count > 1)System.out.println("Method overloading is not allowed!");
+      
+    }
+}
